f1e009b475dc2f56d972c4b947e716c7cd7aba4a
Adam Wight <adam.wight@wikimedia.de>
2022-02-01 23:25:17 +0100

    Start ui container in prod mode
    The dev mode was hanging indefinitely.  Very unfortunately, in prod
    mode we lose the hot-reload when files are changed.

--- docker/entrypoint-ui.sh
+++ docker/entrypoint-ui.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 npm install
-npm run build:dev
+# TODO: "dev" mode automatically reloads--but suffers a bug causing the load to
+# hang indefinitely in this docker environment.
+# npm run build:dev
+npm run build:prod
 npm run server:dev
