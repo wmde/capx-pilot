0653c577aeb3e6d30633f9340cc0f6e71e404983
Adam Wight <adam.wight@wikimedia.de>
2022-05-06 23:18:54 +0200

    use yarn; UI binds to any interface; clean up docker-compose; bump submodules

--- docker/entrypoint-ui.sh
+++ docker/entrypoint-ui.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-npm install
+yarn install
 # TODO: "dev" mode automatically reloads--but suffers a bug causing the load to
 # hang indefinitely in this docker environment.
 # npm run build:dev
-npm run build:prod
-npm run site:prod
+
+yarn run build:dev &
+
+# TODO: What is `site`?
+#yarn run site:dev
 #npm run server:dev
-npm run server:prod
+
+yarn run server:dev
